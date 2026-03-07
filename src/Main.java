73035f8c3188b65f985af2bc161145c8d394ced1
Daniela Carrillo <danlestwalker@gmail.com>
2026-03-07 01:07:33 -0600

    Se incio la interfaz de usuario y se agrego las dependencias Gson

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,64 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner lectura = new Scanner(System.in);
+        int opcion = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String menu = """
+                ***********************************************
+                Sea bienvenido/a al Conversor de Monedas
+                
+                1) Dólar =>> Peso mexicano
+                2) Peso mexicano =>> Dólar
+                3) Dólar =>> Real brasileño
+                4) Real brasileño =>> Dólar
+                5) Dólar =>> Peso colombiano
+                6) Peso colombiano =>> Dólar
+                9) Salir
+                 Elija una opción válida:
+                ***********************************************
+                """;
+
+        while (opcion != 7) {
+            System.out.println(menu);
+
+            if (lectura.hasNextInt()) {
+                opcion = lectura.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Has elegido Dólar a Peso Mexicano");
+                        break;
+
+                    case 2:
+                        System.out.println("Has elegido Peso Mexicano a Dólar");
+                        break;
+
+                    case 3:
+                        System.out.println("Has elegido Dólar a Real Brasileño");
+                        break;
+
+                    case 4:
+                        System.out.println("Has elegido Real Brasileño a Dólar");
+                        break;
+
+                    case 5:
+                        System.out.println("Has elegido Dólar a Peso Colombiano");
+                        break;
+                    case 6:
+                        System.out.println("Has elegido Peso Colombiano a Dólar");
+
+                    case 9:
+                        System.out.println("Finalizando el programa. ¡Gracias por usarlo!");
+                        break;
+                    default:
+                        System.out.println("Opción no válida, por favor intente de nuevo.");
+                }
+            } else {
+                System.out.println("Error: Por favor, ingrese un número entero.");
+                lectura.next();
+            }
         }
     }
 }
